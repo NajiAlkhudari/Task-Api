885ce203434280fd688a34f5e61aa4df82330333
NajiAlkhudari <najikh98@gmail.com>
2025-03-24 14:39:25 +0300

    second commit

--- TaskAlbayan.Common/obj/Debug/net9.0/TaskAlbayan.Common.AssemblyInfo.cs
+++ TaskAlbayan.Common/obj/Debug/net9.0/TaskAlbayan.Common.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskAlbayan.Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9e685243b42651cdcd5aa7cd485c897cf557ad0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskAlbayan.Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskAlbayan.Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
